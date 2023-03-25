f6bac81c66daabe1475324205ebf1ebdecb3645a
Samuel Gonçalves <43956748+samituga@users.noreply.github.com>
2023-03-25 12:08:03 +0000

    #53 Export bard javalin packages (#54)

--- slumber-bard-javalin/src/main/java/module-info.java
+++ slumber-bard-javalin/src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module slumber.bard.javalin {
+    exports io.samituga.slumber.bard.javalin;
+    exports io.samituga.slumber.bard.javalin.exception;
+
     requires slumber.bard;
     requires slumber.ivern;
     requires io.javalin;
