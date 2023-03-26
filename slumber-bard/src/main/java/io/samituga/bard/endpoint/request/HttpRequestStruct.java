e35cd5c38a9f17794e6e13ab540a1bbc263c7c52
samuel.goncalves <samuel.c.goncalves07@gmail.com>
2023-03-26 15:27:30 +0100

    Add RequestBody

--- slumber-bard/src/main/java/io/samituga/bard/endpoint/request/HttpRequestStruct.java
+++ slumber-bard/src/main/java/io/samituga/bard/endpoint/request/HttpRequestStruct.java
@@ -5,19 +5,24 @@ import static io.samituga.slumber.heimer.validator.AssertionUtility.required;
 
 import io.samituga.bard.endpoint.request.type.PathParams;
 import io.samituga.bard.endpoint.request.type.QueryParams;
+import io.samituga.bard.endpoint.request.type.RequestBody;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 record HttpRequestStruct(PathParams pathParams,
                          QueryParams queryParams,
-                         HttpServletRequest request)
+                         HttpServletRequest request,
+                         Optional<RequestBody> requestBody)
       implements HttpRequest {
 
     HttpRequestStruct(PathParams pathParams,
                       QueryParams queryParams,
-                      HttpServletRequest request) {
+                      HttpServletRequest request,
+                      Optional<RequestBody> requestBody) {
         this.pathParams = required("pathParams", pathParams);
         this.queryParams = required("queryParams", queryParams);
         this.request = required("request", request);
+        this.requestBody = required("request", requestBody);
     }
 
     @Override
@@ -25,6 +30,7 @@ record HttpRequestStruct(PathParams pathParams,
         return httpRequestBuilder()
               .pathParams(pathParams)
               .queryParams(queryParams)
-              .request(request);
+              .request(request)
+              .requestBody(requestBody);
     }
 }
