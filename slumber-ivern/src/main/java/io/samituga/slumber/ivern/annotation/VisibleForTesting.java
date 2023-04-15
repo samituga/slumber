f8459e391413ea35732f47c00dc7574e9c366cf0
Samuel Gonçalves <samuel.c.goncalves07@gmail.com>
2023-04-15 12:28:21 +0100

    #70

---
+++ slumber-ivern/src/main/java/io/samituga/slumber/ivern/annotation/VisibleForTesting.java
@@ -0,0 +1,7 @@
+package io.samituga.slumber.ivern.annotation;
+
+/**
+ * Indicates that the target element has more visibility than necessary to be used in tests
+ */
+public @interface VisibleForTesting {
+}
--- slumber-malz/src/main/java/io/samituga/slumber/malz/error/SQLQueryError.java
+++ slumber-malz/src/main/java/io/samituga/slumber/malz/error/SQLQueryError.java
@@ -5,8 +5,8 @@ import static io.samituga.slumber.malz.error.message.SQLQueryErrorMessage.FIND_O
 public class SQLQueryError extends Error {
 
 
-    public SQLQueryError(String condition) { // TODO: 01/01/2023 Generalize
-        super(String.format(FIND_ONE_ERROR.format(), condition)); // TODO: 01/01/2023 Why FIND_ONE_ERROR?
+    public SQLQueryError(String errorMessage) { // TODO: 01/01/2023 Generalize
+        super(errorMessage); // TODO: 01/01/2023 Why FIND_ONE_ERROR?
     }
 
     public SQLQueryError(String condition, Throwable throwable) { // TODO: 01/01/2023 Generalize
