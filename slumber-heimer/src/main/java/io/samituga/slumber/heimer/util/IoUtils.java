7ddab53c3c5fb45ac4f369faa1ba3d3c4ca3324d
Samuel Gonçalves <samuel.c.goncalves07@gmail.com>
2023-04-23 16:15:43 +0100

    Add copyTo() to IoUtils

--- slumber-heimer/src/main/java/io/samituga/slumber/heimer/util/IoUtils.java
+++ slumber-heimer/src/main/java/io/samituga/slumber/heimer/util/IoUtils.java
@@ -3,6 +3,7 @@ package io.samituga.slumber.heimer.util;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class IoUtils {
 
@@ -22,5 +23,20 @@ public class IoUtils {
             outputStream.write(buffer, 0, length);
         }
         return outputStream.toByteArray();
+    }
+
+    public static void copyTo(InputStream inputStream, OutputStream outputStream)
+          throws IOException {
+        copyTo(inputStream, outputStream, BUFFER_SIZE);
+    }
+
+    public static void copyTo(InputStream inputStream, OutputStream outputStream, int bufferSize)
+          throws IOException {
+        var bytesCopied = 0;
+        byte[] buffer = new byte[bufferSize];
+        int length;
+        while ((length = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, length);
+        }
     }
 }
