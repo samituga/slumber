68a67cef042853652b27e26057f4b31476b7412d
Samuel Gonçalves <samuel.c.goncalves07@gmail.com>
2023-04-23 17:46:44 +0100

    Add Jayce module

---
+++ slumber-jayce/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module slumber.jayce {
+    exports io.samituga.jayce;
+
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires slumber.heimer;
+}
