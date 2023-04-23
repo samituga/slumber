4f7417bc2e64fb0d8df7e4ae5c2235492f8e31f4
Samuel Gonçalves <samuel.c.goncalves07@gmail.com>
2023-04-23 16:29:41 +0100

    HttpContext is no more a Structure

--- slumber-bard-javalin/src/main/java/io/samituga/slumber/bard/javalin/mapper/HttpContextMapper.java
+++ slumber-bard-javalin/src/main/java/io/samituga/slumber/bard/javalin/mapper/HttpContextMapper.java
@@ -2,18 +2,14 @@ package io.samituga.slumber.bard.javalin.mapper;
 
 import io.javalin.http.Context;
 import io.samituga.bard.endpoint.context.HttpContext;
-import io.samituga.bard.endpoint.context.HttpContextBuilder;
 import io.samituga.bard.endpoint.request.HttpRequestImpl;
 import io.samituga.bard.endpoint.response.HttpResponseImpl;
 
 public class HttpContextMapper {
 
-
     public static HttpContext toHttpContext(Context javalinCtx) {
-
-        return HttpContextBuilder.httpContextBuilder()
-              .request(new HttpRequestImpl(javalinCtx.req(), javalinCtx.matchedPath()))
-              .response(new HttpResponseImpl(javalinCtx.res()))
-              .build();
+        return new HttpContext(
+              new HttpRequestImpl(javalinCtx.req(), javalinCtx.matchedPath()),
+              new HttpResponseImpl(javalinCtx.res()));
     }
 }
