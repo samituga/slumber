971eb17dab0aaa0333cba3c7b1337ae55d536a70
Samuel Gonçalves <samuel.c.goncalves07@gmail.com>
2023-04-24 11:09:18 +0100

    Update RequestAsTypeBody.java

--- slumber-bard/src/main/java/io/samituga/bard/endpoint/request/type/RequestAsTypeBody.java
+++ slumber-bard/src/main/java/io/samituga/bard/endpoint/request/type/RequestAsTypeBody.java
@@ -7,6 +7,8 @@ import io.samituga.jayce.Json;
 import io.samituga.jayce.JsonException;
 import io.samituga.slumber.heimer.validator.AssertionUtility;
 import io.samituga.slumber.ivern.type.Type;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 
 public class RequestAsTypeBody<T> extends Type<T> {
 
@@ -15,14 +17,16 @@ public class RequestAsTypeBody<T> extends Type<T> {
     }
 
     public static <T> RequestAsTypeBody<T> of(byte[] value, Class<T> type) {
-        return of(new String(value, UTF_8), type);
-    }
-
-    public static <T> RequestAsTypeBody<T> of(String value, Class<T> type) {
         try {
             return new RequestAsTypeBody<>(Json.mapper().readValue(value, type));
         } catch (JsonProcessingException e) {
             throw new JsonException(e);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
         }
+    }
+
+    public static <T> RequestAsTypeBody<T> of(String value, Class<T> type) {
+       return of(value.getBytes(UTF_8), type);
     }
 }
